d62fcefe42782eaa2e35e75a521f9bd73db6347c
John Doe <john@gmail.com>
2021-09-02 17:30:00 +0300

    Heros

---
+++ server.js
@@ -0,0 +1,7 @@
+var superheroes = require('superheroes');
+var mySuperheroName = superheroes.random();
+console.log(mySuperheroName)
+
+var superVillains = require('superVillains');
+var mysuperVillainName = superVillains.random();
+console.log(mysuperVillainName)
